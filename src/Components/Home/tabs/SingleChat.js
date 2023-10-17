77514851f1d7065b3fb86ca0af11f3b9cbf6575f
ms29ash <ms29ash@gmail.com>
2023-10-17 09:19:12 +0530

    change hover in chat

--- src/Components/Home/tabs/SingleChat.js
+++ src/Components/Home/tabs/SingleChat.js
@@ -14,7 +14,7 @@ function SingleChat({ chat }) {
         onClick={() => {
           dispatch({ type: "selectChat", payload: chat });
         }}
-        className="flex   cursor-pointer hover:text-black px-4 py-3 hover:bg-green  items-center group w-full relative  max-w-full"
+        className="flex   cursor-pointer px-4 py-3 hover:bg-black  items-center group w-full relative  max-w-full"
       >
         <img
           className=" mr-3 w-[60px] h-[60px] rounded-full "
@@ -32,10 +32,7 @@ function SingleChat({ chat }) {
             <p className="  truncate">{chat?.latestMsg?.content}</p>
           </div>
           <div className=" right-0 top-0 bottom-0 absolute flex flex-col items-end  justify-center text-sm  w-[30%] z-10  ">
-            <NotificationBadge
-              classes="bg-green text-black group-hover:bg-lightGray group-hover:text-white  mb-4"
-              count={3}
-            />
+            <NotificationBadge classes="bg-green text-black mb-4" count={3} />
 
             <p className="text-xs">
               {chat.latestMsg && (
